e9d86d41e8f4594b1270a8c6f716b8bd9aa40e5e
gitusername <pariginareshkumar@gmail.com>
2020-10-27 13:02:20 +0530

    second commit

--- src/main/java/com/example/jpa/JpaApplication.java
+++ src/main/java/com/example/jpa/JpaApplication.java
@@ -10,6 +10,7 @@ public class JpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpaApplication.class, args);
+		System.out.println("SUcessfully");
 	}
 
 	//@Autowired
