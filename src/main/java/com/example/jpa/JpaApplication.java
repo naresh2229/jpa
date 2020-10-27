8bfb8622f02084138bb6d21f7af175895a5e6304
gitusername <pariginareshkumar@gmail.com>
2020-10-27 13:11:15 +0530

    third commit

--- src/main/java/com/example/jpa/JpaApplication.java
+++ src/main/java/com/example/jpa/JpaApplication.java
@@ -1,23 +1,28 @@
 package com.example.jpa;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.client.RestTemplate;
 
+//import com.example.springjenkins.SpringJenkinsApplicationTests;
+
 @SpringBootApplication
 public class JpaApplication {
+	public static Logger  logger=LoggerFactory.getLogger(JpaApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpaApplication.class, args);
-		System.out.println("SUcessfully");
+		logger.info("SUcessfully jenkins applied");
 	}
 
 	//@Autowired
 	//private RestTemplate rest;
-	RestTemplate restTemplate = new RestTemplate();
+	//RestTemplate restTemplate = new RestTemplate();
 	 
-    String result = restTemplate.getForObject("https://api.bnsf.com:6443/v1/units", String.class);
+  //  String result = restTemplate.getForObject("https://api.bnsf.com:6443/v1/units", String.class);
     
     
 
